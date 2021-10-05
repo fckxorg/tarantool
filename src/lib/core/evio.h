3f70d96d1c838c992289c9deb495795d713ba0bb
mechanik20051988 <mechanik20051988@tarantool.org>
2021-10-05 14:55:29 +0300

    iproto: took out functions related to 'iproto_listen' in separate files.
    The ability to listen multiple sockets will be implemented only in the
    enterprise version of tarantool, so we need to took out all the related
    functions into separate files that will differ in case of the enterprise
    version.
    
    Part of #5928

--- src/lib/core/evio.h
+++ src/lib/core/evio.h
@@ -67,10 +67,17 @@ extern "C" {
  * If a service is not started, but only initialized, no
  * dedicated cleanup/destruction is necessary.
  */
+
+/** \cond public */
+#include <sys/socket.h>
+
+struct ev_io;
+struct ev_loop;
 struct evio_service;
 
 typedef int (*evio_accept_f)(struct evio_service *, int, struct sockaddr *,
 			      socklen_t);
+/** \endcond public */
 
 struct evio_service
 {
@@ -97,13 +104,16 @@ struct evio_service
 
 	/** libev io object for the acceptor socket. */
 	struct ev_io ev;
-	ev_loop *loop;
+	struct ev_loop *loop;
 };
+
+/** \cond public */
 
 /** Initialize the service. Don't bind to the port yet. */
 void
-evio_service_init(ev_loop *loop, struct evio_service *service, const char *name,
-		  evio_accept_f on_accept, void *on_accept_param);
+evio_service_init(struct ev_loop *loop, struct evio_service *service,
+		  const char *name, evio_accept_f on_accept,
+		  void *on_accept_param);
 
 /** Bind service to specified uri */
 int
@@ -125,11 +135,15 @@ evio_service_detach(struct evio_service *service);
 void
 evio_service_stop(struct evio_service *service);
 
+/** \endcond public */
+
 int
 evio_socket(struct ev_io *coio, int domain, int type, int protocol);
 
 void
-evio_close(ev_loop *loop, struct ev_io *evio);
+evio_close(struct ev_loop *loop, struct ev_io *evio);
+
+/** \endcond public */
 
 static inline bool
 evio_service_is_active(struct evio_service *service)
@@ -144,15 +158,15 @@ evio_has_fd(struct ev_io *ev)
 }
 
 static inline void
-evio_timeout_init(ev_loop *loop, ev_tstamp *start, ev_tstamp *delay,
-		  ev_tstamp timeout)
+evio_timeout_init(struct ev_loop *loop, ev_tstamp *start,
+		  ev_tstamp *delay, ev_tstamp timeout)
 {
 	*start = ev_monotonic_now(loop);
 	*delay = timeout;
 }
 
 static inline void
-evio_timeout_update(ev_loop *loop, ev_tstamp *start, ev_tstamp *delay)
+evio_timeout_update(struct ev_loop *loop, ev_tstamp *start, ev_tstamp *delay)
 {
 	ev_tstamp elapsed = ev_monotonic_now(loop) - *start;
 	*start += elapsed;
